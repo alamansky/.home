a73f438ae52618f388ef31f2f9a4bd736dad5de2
Andrew Lamansky <andrewlamansky@gmail.com>
2025-09-09 23:45:55 -0400

    delete unused files

---
+++ .config/env/alias.sh
@@ -0,0 +1,6 @@
+# set interactive shell to zsh
+alias zshell="chsh -s $(which zsh) $(whoami)"
+
+# to specify config explicitly:
+#alias nvim="nvim -u ${XDG_CONFIG_HOME}/nvim/init.lua"
+alias chrome="google-chrome-stable"
