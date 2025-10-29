2de5af75db5d7f9e09f484de050bd8b133b34462
Andrew Lamansky <alamansky@proofpoint.com>
2025-10-28 22:54:54 -0400

    update alias.sh

--- .config/env/alias.sh
+++ .config/env/alias.sh
@@ -6,25 +6,44 @@ alias cnv="cd ~/.home/.config/env && $EDITOR"
 # file shortcuts
 alias zrc="$EDITOR ~/.home/.config/zsh/.zshrc"
 
-# set interactive shell to zsh
-alias zshell="chsh -s $(which zsh) $(whoami)"
-
 # to specify config explicitly:
-#alias nvim="nvim -u ${XDG_CONFIG_HOME}/nvim/init.lua"
+# alias nvim="nvim -u ${XDG_CONFIG_HOME}/nvim/init.lua"
 
-#functions
+# functions
+
+# set interactive shell to zsh
+function zshell {
+  chsh -s $(which zsh) $(whoami)
+}
+
+# copy stdout to Windows clipboard
+# e.g. `echo 'hello world' | copy`
+function copy() {
+  clip.exe
+}
+
+# paste Windows clipboard to stdout
+# e.g. `paste > hello_world.txt`
+function paste() {
+  powershell.exe -c Get-Clipboard
+}
 
 # pretty-print json
-# $1 - json string to pretty print
+# e.g. `cat unformatted.json | pp-json`
 function pp-json() {
-  echo $1 | python3 -m json.tool
+  python3 -m json.tool
+}
+
+# pretty-print json in Windows clipboard
+pp-json-cb() {
+  paste | pp-json | copy
 }
 
 # download repo subdirectory with git
 # $1 - git username/repo, e.g.
 # $2 - subdirectory name
 function git-clone-sparse() {
-  # clone root folder and top-level files only
+# clone root folder and top-level files only
   git clone --filter=blob:none --sparse git@github.com:$1.git
   local username="${1%%/*}"
   local repo="${1##*/}"
@@ -34,6 +53,12 @@ function git-clone-sparse() {
   mv $2 ..
   cd ..
   rm -rf $repo
+}
+
+# serve files in current directory
+# $1 - port to which the server should listen
+function serve() {
+  python3 -m http.server $1
 }
 
 # machine-generated appends
