a0d42b0d961423803dac5d5bec9c7f13f8ac3ec5
Andrew Lamansky <alamansky@proofpoint.com>
2026-08-03 11:18:49 -0400

    Use vars in formatters.lua

--- .config/nvim/lua/overrides/mason/formatters.lua
+++ .config/nvim/lua/overrides/mason/formatters.lua
@@ -1,13 +1,16 @@
 --  https://github.com/stevearc/conform.nvim?tab=readme-ov-file#formatters
+
+local js_formatter = "prettier"
+
 return {
 	lua = { "stylua" },
 	-- custom
 	yaml = { "yamlfmt" },
 	-- css = { "prettier" },
 	-- html = { "prettier" },
-	javascript = { "prettier" },
-	typescript = { "prettier" },
-	javascriptreact = { "prettier" },
-	typescriptreact = { "prettier" },
-	json = { "prettier" },
+	javascript = { js_formatter },
+	typescript = { js_formatter },
+	javascriptreact = { js_formatter },
+	typescriptreact = { js_formatter },
+	json = { js_formatter },
 }
